c474354acb1fae095be9118db0a6722c19b7d16b
KoSeonJe <127813439+KoSeonJe@users.noreply.github.com>
2024-11-09 20:09:10 +0900

    test : 데이터 초기 설정 Test 작성

---
+++ src/test/java/store/FileDataInitializerTest.java
@@ -0,0 +1,36 @@
+package store;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import fixture.StoreFixture;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class FileDataInitializerTest {
+
+    private FileDataInitializer fileDataInitializer = StoreFixture.fileDataInitializer();
+    private InMemoryProductRepository productRepository = StoreFixture.productRepository();
+    private InMemoryPromotionRepository promotionRepository = StoreFixture.promotionRepository();
+
+    @BeforeEach
+    void setUp() {
+        productRepository.clear();
+        promotionRepository.clear();
+    }
+
+    @Test
+    void init() {
+        assertSimpleTest(() -> {
+            fileDataInitializer.init();
+            List<Product> findProducts = productRepository.findByName("콜라");
+            Promotion promotion = promotionRepository.findByName("MD추천상품").orElse(null);
+
+            assertThat(findProducts).isNotNull();
+            assertThat(findProducts).hasSize(2);
+            assertThat(promotion).isNotNull();
+            assertThat(promotion.getBuy()).isEqualTo(1);
+        });
+    }
+}
